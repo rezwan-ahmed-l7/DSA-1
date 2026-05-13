6d972f276815e2e5956d86f0108a7d7b04586cb3
Rezwan <rezwanahmedtorabee@gmail.com>
2026-05-13 17:44:07 +0600

    5th lab added

---
+++ DSA Codes/5th_Lab_Link_List_Delete.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int k)
+    {
+        val = k;
+        next = NULL;
+    }
+};
+
+class List
+{
+public:
+    Node *start = NULL;
+    void ins(int k)
+    {
+        Node *n = new Node(k);
+        if (start == NULL)
+        {
+            start = n;
+        }
+        else
+        {
+            Node *p = start;
+            while (p->next != NULL)
+            {
+                p = p->next;
+            }
+            p->next = n;
+        }
+    }
+
+    void insLast(int k)
+    {
+        Node *n = new Node(k);
+        if (start == NULL)
+        {
+            start = n;
+        }
+        else
+        {
+            n->next = start;
+            start = n;
+        }
+    }
+
+    void deleteFirst()
+    {
+        start = start->next;
+    }
+
+    void deleteLast()
+    {
+        Node *j = start;
+        while (j->next->next != NULL)
+        {
+            j = j->next;
+        }
+        j->next = NULL;
+    }
+
+    void traverse()
+    {
+        Node *p = start;
+        while (p != NULL)
+        {
+            cout << p->val << "\t";
+            p = p->next;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    List t;
+    t.ins(5);
+    t.ins(10);
+    t.ins(3);
+    t.ins(6);
+    t.insLast(9);
+    t.traverse();
+    t.deleteFirst();
+    t.traverse();
+    t.deleteLast();
+    t.traverse();
+    return 0;
+}
